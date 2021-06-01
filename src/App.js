dbe25dcfa34c41985d0f314a48507a0b3623a7da
aniketjamkar007 <aniketjamkar@gmail.com>
2021-06-01 22:30:24 +0530

    React Module 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -27,9 +28,16 @@ function App() {
       date: new Date(2021, 7,24)
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js', expense);
+    expenses.push(expense);
+    console.log(expenses);
+  }
   return (
     <div>
       <h1>Welcome to React Course</h1>
+      <NewExpense  onAddExpense = {addExpenseHandler}/>
       <Expenses expensesData = {expenses}/>
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,19 @@
+import {useState} from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
 
     return(
         <Card className="expenses">
+            <ExpensesFilter onChangeFilter = {filterChangeHandler} selected = {filteredYear} />
             <ExpenseItem title ={props.expensesData[0].title} amount={props.expensesData[0].amount} date={props.expensesData[0].date}></ExpenseItem>
             <ExpenseItem title ={props.expensesData[1].title} amount={props.expensesData[1].amount} date={props.expensesData[1].date}></ExpenseItem>
             <ExpenseItem title ={props.expensesData[2].title} amount={props.expensesData[2].amount} date={props.expensesData[2].date}></ExpenseItem>
